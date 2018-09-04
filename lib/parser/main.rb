d460a71047008a169b279d4d09e503e3639de075
Vladholovchak <vlad.golovchak.1996@gmail.com>
2018-09-04 21:23:27 +0300

    add country_parser for surfing countries_codes

--- lib/parser/main.rb
+++ lib/parser/main.rb
@@ -1,13 +1,9 @@
-require 'nokogiri'
-require 'open-uri'
 require 'pry'
 require 'awesome_print'
-require_relative 'border_parser'
+require_relative 'country_parser'
 
-html = File.read(File.join(File.dirname(__FILE__), 'hungary_i.html'))
-doc = Nokogiri::HTML(html)
-bord_point_pars = BorderParser.new(doc).call
-p = bord_point_pars
-puts p[1]
+f = CountriesParser.new
+f.call
+
 
 
