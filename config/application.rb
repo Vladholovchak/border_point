5dce85435572485d7463215e75ef30dace95d96c
Vladholovchak <vlad.golovchak.1996@gmail.com>
2019-08-07 19:35:06 +0300

    off assets

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,6 @@ module BorderPoint
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.initialize_on_precompile = false
+    config.assets.enabled = false
   end
 end
